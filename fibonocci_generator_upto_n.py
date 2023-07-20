1325679b6fa5d02079177e545db60e4a786df59a
gsainik <sainikhil.gujja@gmail.com>
2023-07-20 21:47:07 +0530

    fibonacci numbers up to n using Generators

---
+++ fibonocci_generator_upto_n.py
@@ -0,0 +1,15 @@
+# To generate fibonacci numbers up to n using Generators
+
+def fibonacci_generator():
+    a,b=0,1
+    while True:
+        yield a
+        a,b=b,a+b
+
+g = fibonacci_generator()
+n= int(input('enter number up to which the fibonacci number are required : '))
+for x in g:
+    if x<= n:
+        print(x)
+    else:
+        break
