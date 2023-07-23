87d84c04cd7ca9f10828703731b11b83be3a2347
gsainik <sainikhil.gujja@gmail.com>
2023-07-23 21:49:51 +0530

    Non prime numbers up to n

---
+++ non_primenumbers_upto_n.py
@@ -0,0 +1,14 @@
+num= int(input("Enter the N value up to which the Non prime numbers to be displayed :"))
+n=2
+while n<=num:
+    count = 0
+    for i in range(2,n):
+        if n%i== 0:
+            count = 1
+        else:
+            continue
+    if count==1:
+        #if count == 0 then it prints all prime number
+        #if count == 1 then it prints all Non prime number
+        print("{}".format(n))
+    n+=1
