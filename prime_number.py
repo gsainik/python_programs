967cb9195f110674e70ad5ce34e74511e48287f4
gsainik <sainikhil.gujja@gmail.com>
2023-08-28 22:25:43 +0530

    prime number

--- prime_number.py
+++ prime_number.py
@@ -1,3 +1,5 @@
+#To check the given number is prime or not
+
 def prime(n):
     if n < 2 :
         print("{} is not a prime number".format(n))
@@ -15,3 +17,21 @@ if count == 2:
     print("{} is prime number".format(n))
 else:
     print("{} is not a prime number".format(n))
+
+
+
+# Alternative 2
+def is_prime(num):
+    if num <= 1:
+        return False
+    else:
+        for i in range(2,num):
+            if num % i == 0:
+                return False
+    return True
+
+num= int(input("Enter a number to check it is Prime or Not: "))
+if is_prime(num):
+    print("{} is prime number".format(num))
+else:
+    print("{} is not a prime number".format(num))
